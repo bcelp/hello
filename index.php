84e4f787cb122318a8ad75c60f0e31cdf4f502c8
bendew <benjamin.grosjean@etu.udamail.fr>
2015-02-02 10:21:26 +0100

    b

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>SUH</title>
+    </head>
+    <body>
+      
+        <?php
+        
+        echo "salut biche!";
+        ?>
+        
+        </body>
+</html>
